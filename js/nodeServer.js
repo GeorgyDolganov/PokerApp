4e3cdd105ff04be24b8844ae7bb9a7e189ed55e6
Georgy Dolganov <mr.craaazy@gmail.com>
2020-06-06 09:26:02 +0500

    Basic layout for the app

---
+++ js/nodeServer.js
@@ -0,0 +1,49 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log('socket connected');
+
+    socket.on('disconnect', function () {
+        console.log('socket disconnected');
+    });
+
+    socket.emit('text', 'Вы успешно подключились к серверу');
+});
+
+server.listen(3000);
+
+// var server = require('http').createServer();
+// var io = require('socket.io')(server);
+
+// io.sockets.on('connection', function (socket) {
+//     console.log('socket connected');
+
+//     socket.on('disconnect', function () {
+//         console.log('socket disconnected');
+//     });
+
+//     socket.emit('text', 'Подсоединение выполненно успешно');
+// });
+
+// server.listen(3000);
+
+// var socket = require( 'socket.io' );
+// var express = require( 'express' );
+// var http = require( 'http' );
+
+// var app = express();
+// var server = http.createServer( app );
+
+// var io = socket.listen( server );
+
+// io.sockets.on( 'connection', function( client ) {
+// 	console.log( "New client !"+client.handshake.sessionID);
+	
+// 	client.on( 'message', function( data ) {
+// 		console.log( 'Message received ' + data.message );
+// 		io.sockets.emit( 'message', { message: data.message } );
+// 	});
+// });
+
+// server.listen( 1034 );
